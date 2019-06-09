55bfedd2354e6ee1a0e8f92fe7822ba0fcfb5404
Erin Eland <erin@erineland.com>
2019-06-09 15:29:05 +0100

    Removign comments.

--- src/people-filter.js
+++ src/people-filter.js
@@ -1,23 +1,13 @@
-storeAndGroupByGender = (personToFilter, filteredPeopleGroupedByGender, ruleToUse) => {
+filterAndGroupByGender = (personToFilter, filteredPeopleGroupedByGender, ruleToUse) => {
     if (eval(ruleToUse)) {
-        // console.log(`Person to filter gender is: ${personToFilter.gender}`);
-        // console.log(`filteredPeopleGroupedByGender[${personToFilter.gender}] is: ${JSON.stringify(filteredPeopleGroupedByGender[personToFilter.gender])}`);
         if (filteredPeopleGroupedByGender[personToFilter.gender]) {
-            // console.log(`${personToFilter.gender} Gender existed, adding to it`);
-
             const existingArray = filteredPeopleGroupedByGender[personToFilter.gender];
-            // console.log(`existing array was: ${JSON.stringify(existingArray)}`);
-
             existingArray.push(personToFilter);
             filteredPeopleGroupedByGender[personToFilter.gender] = existingArray;
-
-            // console.log(`filteredPeopleGroupedByGender[${personToFilter.gender}] is now: ${filteredPeopleGroupedByGender[personToFilter.gender]}`);
         } else {
-            // console.log(`${personToFilter.gender} Gender didn't exist, creating it`);
             const newGenderArray = [];
             newGenderArray.push(personToFilter);
             filteredPeopleGroupedByGender[personToFilter.gender] = newGenderArray;
-            // console.log(`filteredPeopleGroupedByGender is now: ${JSON.stringify(filteredPeopleGroupedByGender)}`);
         }
     }
 }
@@ -32,16 +22,13 @@ module.exports = (peopleToFilter, customRules) => {
         rulesToUse.push('personToFilter.age >= 30 && personToFilter.age <= 40');
     }
 
-    // TODO: Have array of rules to apply to code.
     const filteredPeopleGroupedByGender = {};
 
     peopleToFilter.forEach(personToFilter => {
         rulesToUse.forEach(rule => {
-            storeAndGroupByGender(personToFilter, filteredPeopleGroupedByGender, rule);
+            filterAndGroupByGender(personToFilter, filteredPeopleGroupedByGender, rule);
         })
     });
-
-    // console.log(`The filtered array is: ${JSON.stringify(filteredPeopleGroupedByGender)}`);
 
     return filteredPeopleGroupedByGender;
 }
