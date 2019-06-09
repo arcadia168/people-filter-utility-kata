e755cdce756cd08cfea52985e4a5ea4f27601138
Erin Eland <erin@erineland.com>
2019-06-09 15:21:29 +0100

    Refactoring for re-usability

--- src/people-filter.js
+++ src/people-filter.js
@@ -1,3 +1,25 @@
+storeAndGroupByGender = (personToFilter, filteredPeopleGroupedByGender) => {
+    // console.log(`Person to filter gender is: ${personToFilter.gender}`);
+    // console.log(`filteredPeopleGroupedByGender[${personToFilter.gender}] is: ${JSON.stringify(filteredPeopleGroupedByGender[personToFilter.gender])}`);
+    if (filteredPeopleGroupedByGender[personToFilter.gender]) {
+        // console.log(`${personToFilter.gender} Gender existed, adding to it`);
+
+        const existingArray = filteredPeopleGroupedByGender[personToFilter.gender];
+        // console.log(`existing array was: ${JSON.stringify(existingArray)}`);
+
+        existingArray.push(personToFilter);
+        filteredPeopleGroupedByGender[personToFilter.gender] = existingArray;
+
+        // console.log(`filteredPeopleGroupedByGender[${personToFilter.gender}] is now: ${filteredPeopleGroupedByGender[personToFilter.gender]}`);
+    } else {
+        // console.log(`${personToFilter.gender} Gender didn't exist, creating it`);
+        const newGenderArray = [];
+        newGenderArray.push(personToFilter);
+        filteredPeopleGroupedByGender[personToFilter.gender] = newGenderArray;
+        // console.log(`filteredPeopleGroupedByGender is now: ${JSON.stringify(filteredPeopleGroupedByGender)}`);
+    }
+}
+
 module.exports = peopleToFilter => {
     if (!peopleToFilter) {
         throw new Error('Please supply a valid list of people objects.');
@@ -10,25 +32,7 @@ module.exports = peopleToFilter => {
         // console.log(`Person being filtered is: ${JSON.stringify(personToFilter)}`);
 
         if (personToFilter.age >= 30 && personToFilter.age <= 40) {
-            // console.log(`Person to filter gender is: ${personToFilter.gender}`);
-            // console.log(`filteredPeopleGroupedByGender[${personToFilter.gender}] is: ${JSON.stringify(filteredPeopleGroupedByGender[personToFilter.gender])}`);
-            if (filteredPeopleGroupedByGender[personToFilter.gender]) {
-                // console.log(`${personToFilter.gender} Gender existed, adding to it`);
-
-                const existingArray = filteredPeopleGroupedByGender[personToFilter.gender];
-                // console.log(`existing array was: ${JSON.stringify(existingArray)}`);
-
-                existingArray.push(personToFilter);
-                filteredPeopleGroupedByGender[personToFilter.gender] = existingArray;
-
-                // console.log(`filteredPeopleGroupedByGender[${personToFilter.gender}] is now: ${filteredPeopleGroupedByGender[personToFilter.gender]}`);
-            } else {
-                // console.log(`${personToFilter.gender} Gender didn't exist, creating it`);
-                const newGenderArray = [];
-                newGenderArray.push(personToFilter);
-                filteredPeopleGroupedByGender[personToFilter.gender] = newGenderArray;
-                // console.log(`filteredPeopleGroupedByGender is now: ${JSON.stringify(filteredPeopleGroupedByGender)}`);
-            }
+            storeAndGroupByGender(personToFilter, filteredPeopleGroupedByGender);
         }
     });
 
